ced6d7462110d8af17fd8db57d1af1f376f41802
Peter <peterjohansson130@gmail.com>
2022-03-07 13:45:43 +0100

    added simulated login (not finished)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import Login from './components/pages/login/Login';
+import Login from './pages/login/Login';
 
 export const Header = () => {
   return (
--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,4 +1,20 @@
+
+
 const Login = () => {
-    return <h1>TODO login</h1>;
+    const nameArr = ["kalle", "anna", "bertil", "qwerty"];
+
+    return (
+      <form action="/action_page.php">
+        <label for="employee">Choose a car:</label>
+        <select name="Employees" id="employee">
+        <option value="kalle">kalle</option>
+        <option value="anna">anna</option>
+        <option value="bertil">bertil</option>
+        <option value="qwerty">qwerty</option>
+        </select>
+        <br></br>
+        <input type="submit" value="Submit"/>
+      </form>
+    );
   };
 export default Login;
