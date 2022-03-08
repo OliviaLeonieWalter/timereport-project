9975bcc12359b8e6a0c27a0e7ffe24b96c50d8cc
Olivia Walter <olivia.walter@edu.edugrade.se>
2022-03-08 14:12:49 +0100

    trash init

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,22 +1,26 @@
+import { giveMeAnswer } from "../../../services/notion";
 
 const Login = () => {
-   
-  function getEmployee(){
+  useEffect(() => {
+    giveMeAnswer();
+  }, []);
+
+  function getEmployee() {
     var employeeLoggedIn = document.getElementById("employeeDropdown");
 
     console.log(employeeLoggedIn.value);
   }
 
-    return (
-      <div id="loginSection">
-        <select id="employeeDropdown" name="">
-          <option value="Kalle">Kalle</option>
-          <option value="Anna">Anna</option>
-        </select>
-         
-        <button id="fetchEmployeeButton" onClick={getEmployee}>Login</button>
-      </div> 
-    );
-  };
+  return (
+    <div id="loginSection">
+      <select id="employeeDropdown" name="">
+        <option value="Kalle">Kalle</option>
+        <option value="Anna">Anna</option>
+      </select>
+
+      <button id="fetchEmployeeButton" onClick={getEmployee}>Login</button>
+    </div>
+  );
+};
 
 export default Login;
