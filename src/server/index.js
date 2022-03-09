ad3797338dd6c8968b69f16a476497a0581402e4
Simon Johansson <99795909+Simxn999@users.noreply.github.com>
2022-03-09 22:11:47 +0100

    Changed port 5000 -> 3001 for backend-server

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,21 @@ import express from "express";
 import { Client } from "@notionhq/client";
 
 const app = express();
-const port = 5000;
+const port = 3001;
 
-const TOKEN = "secret_iAH7qE3H2iPsk4oBTiTN99lqHR71VyEYoNHD1yY2jvJ";
-const notion = new Client({ auth: TOKEN });
+const token = "secret_iAH7qE3H2iPsk4oBTiTN99lqHR71VyEYoNHD1yY2jvJ";
+const notion = new Client({ auth: token });
 
 app.listen(port, console.log(`Server created on port: ${port}`));
 
 app.get('^/database/:databaseId(*)', (req, res) => {
   notion.databases.retrieve({ database_id: req.params.databaseId })
+    .then(database => res.send(database))
+    .catch(() => res.sendStatus(404));
+});
+
+app.get('^/databases/', (req, res) => {
+  notion.databases.query('page')
     .then(database => res.send(database))
     .catch(() => res.sendStatus(404));
 });
