74f0d3517daa737d7714ffc43a3fdff090167ae1
Olivia Walter <olivia.walter@edu.edugrade.se>
2022-03-14 13:41:15 +0100

    init roles

---
+++ src/components/pages/roles/Roles.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+
+// Basic = 0, Project Owner = 1, Boss = 2
+export function Roles({user}) {
+    const [userroll, setUserroll] = useState(false); 
+
+    useEffect(() => { 
+
+
+
+    })
+    return(
+        <main>
+            <h1>Hi</h1>
+        </main>
+    )
+}
